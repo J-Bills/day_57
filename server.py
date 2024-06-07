512ab8be41a1eec20ce7fc5113be1ce5ea08c774
J-Bills <billsjamal@gmail.com>
2024-06-07 12:38:58 -0700

    Used python code with template

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+import random
+import datetime as dt
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #return '<h1>Hello World<h1/>'
-    
-    #returning the html template that we created
-    return render_template('index.html')
+    rand_number = random.randint(1,10)
+    result = dt.datetime.now()
+    result = result.year
+    return render_template('index.html', num=rand_number, time=result)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
